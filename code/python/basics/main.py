8cc8fc04d46c7f6d6c4fd510dcb380dd8770dcd6
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2020-11-20 22:56:27 +0100

    update

--- code/python/basics/main.py
+++ code/python/basics/main.py
@@ -1,33 +1,20 @@
-import timeit
+class Car:
+    # class attribute
+    wheels = 4
 
-def change_list(numbers):
-    """This changes a passed list into this function"""
-    numbers = [40, 50, 60]
-    return
+    # instance attribute
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
 
 
 if __name__ == '__main__':
-    squares = {1: 1, 3: 9, 5: 25, 7: 49, 9: 81}
+    m3 = Car('Bmw', 'M3')
+    tsla = Car('Tesla', 'Roadster')
 
-    # membership tests for key only
-    print(1 in squares)
-    print(2 not in squares)
-    print(49 in squares)
+    print("Wheels={}".format(m3.__class__.wheels))
+    print("Brand={}, Model={}".format(m3.brand, m3.model))
 
-    # Iterating through keys
-    for i in squares:
-        print(squares[i])
-
-    # Iterating through keys and values
-    for k, v in squares.items():
-        print(k, v)
-
-
-
-
-
-
-
-
-
+    print("Wheels={}".format(tsla.__class__.wheels))
+    print("Brand={}, Model={}".format(tsla.brand, tsla.model))
 
