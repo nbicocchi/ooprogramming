4bedda0d06e0121816d90b6e8c33b444ce37783f
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2020-11-24 23:30:07 +0100

    update

--- code/python/basics/main.py
+++ code/python/basics/main.py
@@ -1,19 +1,3 @@
-class Car:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
-
-    def __repr__(self):
-        return '{} {}'.format(self.brand, self.model)
-
-
-if __name__ == '__main__':
-    x = 4+3j
-    print(x.real, "+", x.imag, 'i')
-    x = 4.5
-    print(x.is_integer())
-    x = 4.0
-    print(x.is_integer())
 
 
 
