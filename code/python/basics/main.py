9b927d82e691066c85c4078aa35ce8e9fe047072
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2020-11-25 15:08:24 +0100

    update

--- code/python/basics/main.py
+++ code/python/basics/main.py
@@ -1,3 +1,11 @@
+name = ' python '
+print('\'{}\''.format(name.rstrip()))
+print('\'{}\''.format(name.lstrip()))
+print('\'{}\''.format(name.strip()))
 
+name = 'python'
+print('\'{}\''.format(name.rjust(10)))
+print('\'{}\''.format(name.ljust(10)))
+print('\'{}\''.format(name.center(10)))
 
 
